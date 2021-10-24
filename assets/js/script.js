d1d6a4180947529ae66ae1f0cb087ace3dfbf012
Naomi Eckhoff <nsuibhne@gmail.com>
2021-10-24 09:16:17 -0500

    finished up the code for saving changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
+var scheduleSave = [];
 var today = moment().format("dddd") + ", " + moment().format("MMMM") + " " + moment().format("Do");
 
 document.getElementById("currentDay").textContent = today;
 
 for (var i = 9; i < 18; i++) {
   // create elements that make up time slots
-  var timeLi = $("<li>").addClass("schedule-list");
-  var timeDivLeft = $("<div>").addClass("time-static");
+  var timeLi = $("<li>").addClass("schedule-list row");
+  var timeDivLeft = $("<div>").addClass("time-static hour");
   var timePLeft = $("<p>")
     .addClass("time-static-p static-time-" + i)
     .text(i + ":00");
@@ -13,10 +14,11 @@ for (var i = 9; i < 18; i++) {
     var timePLeft = $("<p>")
       .text("0" + i + ":00");
   };
-  var timeDivRight = $("<div>").addClass("scheduled-activity");
+  var timeDivRight = $("<div>").addClass("scheduled-activity time-block");
   var timePRight = $("<p>")
-    .addClass("m-1")
-    .text("aaaaa");
+    .addClass("scheduled-todo textarea")
+    .attr("id", "row" + (i - 9))
+    .text("Placeholder text");
   timeDivLeft.append(timePLeft);
   timeDivRight.append(timePRight);
   // append span and p element to parent li
@@ -26,16 +28,82 @@ for (var i = 9; i < 18; i++) {
   $(".time-list").append(timeLi);
 }
 
+var saveSchedule = function () {
+  for (i = 0; i < 9; i++) {
+    scheduleSave[i] = document.getElementById("row" + i).textContent;
+  }
+  localStorage.setItem("scheduleSave", JSON.stringify(scheduleSave));
+}
+
+var loadSchedule = function () {
+  scheduleSave = JSON.parse(localStorage.getItem("scheduleSave"));
+  if (!scheduleSave) {
+    scheduleSave = [];
+  }
+
+  for (i = 0; i < 9; i++) {
+    $("#row" + i).text(scheduleSave[i]);
+  }
+}
+
+
 function timeCheck() {
   $(".schedule-list").each(function () {
     if ($(".time-static-p") < moment().format("HH") + ":" + moment().format("mm")) {
-      $(".schedule-list").addClass("overdue");
-    }
-    setInterval(function () {
-      timeCheck();
-    }, 1800000);
+      $(".schedule-list").addClass("past");
+    } else
+      if ($(".time-static-p") === moment().format("HH")) {
+        $(".schedule-list").addClass("present");
+      } else {
+        $(".schedule-list").addClass("future");
+      }
+
   });
 };
 
 timeCheck();
 
+setInterval(function () {
+  timeCheck();
+}, 1800000);
+
+var idStorage;
+// task text was clicked
+$(".scheduled-activity").on("click", "p", function () {
+  // get current text of p element
+  var text = $(this)
+    .text()
+    .trim();
+
+  idStorage = $(this).attr("id");
+
+  // replace p element with a new textarea
+  var textInput = $("<textarea>").addClass("form-control").val(text);
+  $(this).replaceWith(textInput);
+
+  // auto focus new element
+  textInput.trigger("focus");
+});
+
+// editable field was un-focused
+$(".scheduled-activity").on("blur", "textarea", function () {
+  // get current value of textarea
+  var textVal = $(this).val();
+  console.log(textVal);
+  var idTemp = (idStorage.replace("row", ""));
+  // update task in array and re-save to localstorage
+  scheduleSave[idTemp] = textVal;
+
+  // recreate p element
+  var scheduleP = $("<p>")
+    .addClass("scheduled-todo")
+    .attr("id", idStorage)
+    .text(textVal);
+
+  // replace textarea with new content
+  $(this).replaceWith(scheduleP);
+
+  saveSchedule();
+});
+
+loadSchedule();
